02fe76954615e3371fb8993eeff51ca38c6c246d
Michael Williams <micwilli@microsoft.com>
2022-02-18 17:58:11 -0800

    remove old version, update deploy script

--- tools/cvpdeployment/Deploy-SampleClaimsProvider.ps1
+++ tools/cvpdeployment/Deploy-SampleClaimsProvider.ps1
@@ -14,14 +14,22 @@ PARAM (
 )
 
 Set-AzContext -SubscriptionId $SubscriptionId
-$samplesClaimsProviderPath = $PSScriptRoot + "/SamplesClaimsProvider"
 
+# All paths we'll use
+$sampleClaimsProviderPath = $PSScriptRoot + "/SampleClaimsProvider"
+$sampleClaimsProviderSolutionPath = $PSScriptRoot + "/SampleClaimsProvider/SampleClaimsProvider.sln"
+$sampleClaimsProviderBinariesPath = $sampleClaimsProviderPath + "/SampleClaimsProvider/bin/Debug"
+$zipPath = $PSScriptRoot + "/sampleClaimProvider.zip"
+$functionAppArmTemplatePath = $PSScriptRoot + "/FunctionAppARMTemplate.json"
+
+# Ensure resource group exists
 $rg = Get-AzResourceGroup -Name $ResourceGroupName
 if ($null -eq $rg)
 {
     throw "Resource group $ResourceGroupName does not exist"
 }
 
+# Ensure function app exists, otherwise create it if specified
 $functionApp = Get-AzFunctionApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName
 if ($null -eq $functionApp)
 {
@@ -30,12 +38,46 @@ if ($null -eq $functionApp)
         throw "Function app $FunctionAppName does not exist in $ResourceGroupName"
     }
 
-    $functionAppArmTemplatePath = $PSScriptRoot + "/FunctionAppARMTemplate.json"
     $armParameters = @{
         "appName" = $FunctionAppName
     }
     New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateUri $functionAppArmTemplatePath -TemplateParameterObject $armParameters
 }
 
-$samplesClaimsProviderPath = $PSScriptRoot + "/SamplesClaimsProvider"
+
 # Build Sample Claims Provider
+if ((Test-Path $sampleClaimsProviderSolutionPath) -eq $false)
+{
+    throw "Unable to find SampleClaimsProvider.sln at $sampleClaimsProviderSolutionPath"
+}
+
+Write-Host "Building SampleClaimsProvider"
+$buildResult = dotnet build $sampleClaimsProviderSolutionPath /p:Configuration=Debug /t:"Restore;Build" *>&1
+if ($LASTEXITCODE -eq 0) {
+    Write-Host ("Build completed successfully.")
+}
+else {
+    throw ("Build failed.\n$($buildResult -join [System.Environment]::NewLine)")
+}
+
+# Package and Deploy Sample Claims Provider
+try
+{
+    Write-Host "Packaging SampleClaimsProvider"
+    if ((Test-Path $sampleClaimsProviderBinariesPath) -eq $false)
+    {
+        throw "Unable to find SampleClaimsProvider binaries at $sampleClaimsProviderPath"
+    }
+    Compress-Archive -Path "$sampleClaimsProviderBinariesPath/*" -Update -DestinationPath $zipPath
+
+    Write-Host "Uploading SampleClaimsProvider to $FunctionAppName"
+    Publish-AzWebApp -ResourceGroupName $ResourceGroupName -Name $FunctionAppName -ArchivePath $zipPath -Force
+    Write-Host "Finised deploying SampleClaimsProvider"
+}
+finally 
+{
+    if ((Test-Path -Path $zipPath) -eq $true)
+    {
+        Remove-Item -Path $zipPath -Force
+    }
+}
