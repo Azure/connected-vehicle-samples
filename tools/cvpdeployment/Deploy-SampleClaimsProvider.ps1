75ed521a31ad0de90ed3ec45b12d198349e0d5a9
Michael Williams <micwilli@microsoft.com>
2022-02-23 09:58:54 -0800

    add examples, rename parameter to CreateClaimsProviderResources

--- tools/cvpdeployment/Deploy-SampleClaimsProvider.ps1
+++ tools/cvpdeployment/Deploy-SampleClaimsProvider.ps1
@@ -1,10 +1,10 @@
 <#
 .SYNOPSIS
-    Deploys the sample claims provider to the given function app. If -CreateFunctionApp is provided it will deploy a new function app and cosmos db to use for the sample claims provider.
+    Deploys the sample claims provider to the given function app. If -CreateClaimsProviderResources is provided it will deploy a new function app and cosmos db to use for the sample claims provider.
 
 .DESCRIPTION
     Deploys the sample claims provider to the given function app. The sample claims provider requires a Cosmos DB instance to run and the function app it is deployed to must have the application setting 'ConnectionStrings__CosmosDBConnectionString' set with a connection string to that CosmosDB instance.
-    If the -CreateFunctionApp parameter is provided then an ARM template will be ran deploying a new function app and Cosmos DB instance. In addition it will configure the connection strings between the function app and the Cosmos DB.
+    If the -CreateClaimsProviderResources parameter is provided then an ARM template will be ran deploying a new function app and Cosmos DB instance. In addition it will configure the connection strings between the function app and the Cosmos DB.
 
 .PARAMETER SubscriptionId
     The id of the subscription to deploy the sample claims provider to.
@@ -16,9 +16,24 @@
     The name of the function app to deploy the sample claims provider to.
     If -CreateFunctionApp is provided this will be the name of the newly created function app and supporting resources.
 
-.PARAMETER CreateFunctionApp
-    If provided an ARM template will run deploying a new function app, cosmos db, and required supporting resources.
+.PARAMETER CreateClaimsProviderResources
+    If provided, an ARM template will run deploying a new function app, cosmos db, and required supporting resources.
     In addition the function app will be configured appropriately with a connection string to the cosmos db instance.
+    The sample claims provider will then be deployed to this newly created function app
+
+.EXAMPLE
+    The simplest use case starts with an existing resource group and no function app deployed.
+    This example will create a new function app called 'sample-claims-provider' in the 'contoso-rg' under the subscription 0d82dd27-098f-414c-9259-2436d030609d.
+    In addition all supporting resources, such as a cosmos db, are created and the sample claims provider will be deployed to the newly created 'smaple-claims-provider' function app.
+    Once deployed the sample claims provider should be fully functional.
+
+    PS C:\> ./Deploy-SampleClaimsProvider.ps1 -SubscriptionId 00000000-1111-2222-3333-444444444444 -ResourceGroupName contoso-rg -FunctionAppName sample-claims-provider -CreateClaimsProviderResources
+
+.EXAMPLE
+    If you already have a function app setup that you would prefer to use, you can omit the -CreateClaimsProviderResources paramter.
+    In this case the sample claims provider will be deployed to the function app 'my-existing-function-app'. No additional setup or configuration will be done.
+
+    PS C:\> ./Deploy-SampleClaimsProvider.ps1 -SubscriptionId 00000000-1111-2222-3333-444444444444 -ResourceGroupName contoso-rg -FunctionAppName my-existing-function-app
 #>
 [cmdletbinding()]
 PARAM (
@@ -32,7 +47,7 @@ PARAM (
     [string] $FunctionAppName,
 
     [parameter(mandatory = $false)]
-    [switch] $CreateFunctionApp
+    [switch] $CreateClaimsProviderResources
 )
 $ErrorActionPreference = "Stop"
 Set-AzContext -SubscriptionId $SubscriptionId
@@ -52,7 +67,7 @@ if ($null -eq $rg)
 }
 
 # Create function app if specified
-if ($CreateFunctionApp)
+if ($CreateClaimsProviderResources)
 {
     Write-Host "Deploying claims provider function app and cosmos database"
     $armParameters = @{
